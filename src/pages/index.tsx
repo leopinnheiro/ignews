0c8b85498d7dbbefd38c8c17e6040e1235f3dfd6
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2021-06-24 05:34:02 -0300

    Deixando as informaçoes da pagina Home estaticas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import avatarImg from '../../public/images/avatar.svg';
@@ -36,7 +36,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IhThvIIL1OOzMl68VdE9PnO');
 
   const product = {
@@ -51,6 +51,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 horas
   }
 };
