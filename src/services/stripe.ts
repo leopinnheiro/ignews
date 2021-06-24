6cdce6b46d1c7d7e025cbdffadd14daa6044fd09
Leonardo Pinheiro <leo.pinnheiro@gmail.com>
2021-06-24 05:21:05 -0300

    Melhorando importaçao dos dados do package

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,12 +1,12 @@
 import Stripe from "stripe";
-import { version, name } from "../../package.json";
+import packageInfo from "../../package.json";
 
 const apiKey = process.env.STRIPE_API_KEY ?? "";
 
 export const stripe = new Stripe(apiKey, {
   apiVersion: "2020-08-27",
   appInfo: {
-    name,
-    version,
+    name: packageInfo.name,
+    version: packageInfo.version,
   },
 });
